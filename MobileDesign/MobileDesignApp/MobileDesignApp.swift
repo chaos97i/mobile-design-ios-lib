bb84b5dad656e223e52c35632a4a98756a10c9b6
Hosea Li <hosea.h.c.li@gmail.com>
2023-12-05 22:42:07 +0800

    Add sample app

---
+++ MobileDesign/MobileDesignApp/MobileDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDesignApp.swift
+//  MobileDesignApp
+//
+//  Created by Hosea H C Li on 2023/12/5.
+//
+
+import SwiftUI
+
+@main
+struct MobileDesignAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
